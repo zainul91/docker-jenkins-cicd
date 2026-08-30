c11e625e7e187ba0975dd9f4634f579824f0f1e9
zainul91 <zainulabedin997@gmail.com>
2026-08-30 21:39:00 +0530

    Initial Maven project

---
+++ jenkins-maven-demo/src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Zainul!");
+        System.out.println("Maven build is working successfully.");
+        System.out.println("Jenkins build is working successfully.");
+
+    }
+}
