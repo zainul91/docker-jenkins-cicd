b924bf4a754c71f1ea6de11f0d2b3e48cbd40bf7
zainul91 <zainulabedin997@gmail.com>
2026-08-30 23:34:23 +0530

    Update App.java

--- jenkins-maven-demo/src/main/java/com/example/App.java
+++ jenkins-maven-demo/src/main/java/com/example/App.java
@@ -7,6 +7,7 @@ public class App {
         System.out.println("Hello Zainul!");
         System.out.println("Maven build is working successfully.");
         System.out.println("Jenkins build is working successfully.");
+        System.out.println("Jenkins Poll SCM Test"); 
 
     }
 }
