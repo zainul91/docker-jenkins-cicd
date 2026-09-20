e6e3748c6084cb06442948da6567fcb96a3047d1
zainul91 <zainulabedin997@gmail.com>
2026-09-20 23:22:36 +0530

    Update application message

--- docker-jenkins-cicd/docker-jenkins-cicd/src/main/java/com/zainul/App.java
+++ docker-jenkins-cicd/docker-jenkins-cicd/src/main/java/com/zainul/App.java
@@ -2,6 +2,6 @@ package com.zainul;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Zainul - Jenkins Docker CI/CD");
+    System.out.println("Hello Zainul - CI/CD Pipeline Working Successfully!");
     }
 }
